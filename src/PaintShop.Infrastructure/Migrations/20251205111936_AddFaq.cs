202ed1e828d2e3a02170a70f15b2313d63af63c3
Shuji <marcellalter@gmail.com>
2025-12-05 12:57:17 +0100

    migrations

---
+++ src/PaintShop.Infrastructure/Migrations/20251205111936_AddFaq.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaintShop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFaq : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsInside",
+                table: "Product",
+                type: "bit(1)",
+                nullable: false,
+                oldClrType: typeof(sbyte),
+                oldType: "tinyint(1)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Paid",
+                table: "Orders",
+                type: "bit(1)",
+                nullable: false,
+                oldClrType: typeof(sbyte),
+                oldType: "tinyint(1)");
+
+            migrationBuilder.CreateTable(
+                name: "Faq",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Question = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Answer = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faq", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Faq");
+
+            migrationBuilder.AlterColumn<sbyte>(
+                name: "IsInside",
+                table: "Product",
+                type: "tinyint(1)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit(1)");
+
+            migrationBuilder.AlterColumn<sbyte>(
+                name: "Paid",
+                table: "Orders",
+                type: "tinyint(1)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit(1)");
+        }
+    }
+}
---
+++ src/PaintShop.Infrastructure/Migrations/20251205112812_AddCopuon.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaintShop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCopuon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupon",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Value = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DiscountAmount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupon", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupon");
+        }
+    }
+}
